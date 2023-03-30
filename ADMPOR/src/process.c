4da7a08cc30deeca6397220ac4e5262e5488efa1
edupro26 <edupro26@gmail.com>
2023-03-31 00:25:15 +0100

    Added TODO methods in process.c

---
+++ ADMPOR/src/process.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "../include/process.h"
+
+int launch_client(int client_id, struct comm_buffers* buffers, struct main_data* data){
+    //TODO
+}
+
+int launch_interm(int interm_id, struct comm_buffers* buffers, struct main_data* data){
+    //TODO
+}
+
+int launch_enterp(int enterp_id, struct comm_buffers* buffers, struct main_data* data){
+    //TODO
+}
+
+int wait_process(int process_id){
+    //TODO
+}
