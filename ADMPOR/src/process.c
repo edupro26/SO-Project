56819487b0111a58ca2c60a9f39189ebefa24338
edupro26 <edupro26@gmail.com>
2023-03-31 00:40:14 +0100

    Wrote launch_client in process.c

--- ADMPOR/src/process.c
+++ ADMPOR/src/process.c
@@ -2,11 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #include "../include/process.h"
+#include "../include/client.h"
 
 int launch_client(int client_id, struct comm_buffers* buffers, struct main_data* data){
-    //TODO
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("pid");
+        exit(1);
+    }
+    if(pid == 0){
+        int client = execute_client(client_id, buffers, data);
+        exit(client);
+    }
+    else{
+        return pid;
+    }
 }
 
 int launch_interm(int interm_id, struct comm_buffers* buffers, struct main_data* data){
