e39fd8dc68a1769e5f8f8a9dade46ddea0965b4f
edupro26 <edupro26@gmail.com>
2023-03-31 01:18:47 +0100

    Finished process.c (Testing required)

--- ADMPOR/src/process.c
+++ ADMPOR/src/process.c
@@ -6,11 +6,13 @@
 
 #include "../include/process.h"
 #include "../include/client.h"
+#include "../include/intermediary.h"
+#include "../include/enterprise.h"
 
 int launch_client(int client_id, struct comm_buffers* buffers, struct main_data* data){
     pid_t pid = fork();
     if(pid == -1){
-        perror("pid");
+        perror("launch error");
         exit(1);
     }
     if(pid == 0){
@@ -23,13 +25,46 @@ int launch_client(int client_id, struct comm_buffers* buffers, struct main_data*
 }
 
 int launch_interm(int interm_id, struct comm_buffers* buffers, struct main_data* data){
-    //TODO
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("launch error");
+        exit(1);
+    }
+    if(pid == 0){
+        int interm = execute_intermediary(interm_id, buffers, data);
+        exit(interm);
+    }
+    else{
+        return pid;
+    }
 }
 
 int launch_enterp(int enterp_id, struct comm_buffers* buffers, struct main_data* data){
-    //TODO
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("launch error");
+        exit(1);
+    }
+    if(pid == 0){
+        int enterp = execute_enterprise(enterp_id, buffers, data);
+        exit(enterp);
+    }
+    else{
+        return pid;
+    }
 }
 
 int wait_process(int process_id){
-    //TODO
+    int status;
+    pid_t w_pid = waitpid(process_id, &status, 0);
+
+    if(w_pid == -1){
+        perror("waitpid");
+        exit(1);
+    }
+
+    if(WIFEXITED(status)){
+        int exit_status = WEXITSTATUS(status);
+        return exit_status;
+    }
 }
