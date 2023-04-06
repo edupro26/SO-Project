2d66c348b9b3e705bd2083acd01bc96f60c8f278
edupro26 <edupro26@gmail.com>
2023-04-06 18:47:23 +0100

    Started client.c

--- ADMPOR/src/client.c
+++ ADMPOR/src/client.c
@@ -4,18 +4,32 @@
 
 #include "client.h"
 
-int execute_client(int client_id, struct comm_buffers* buffers, struct main_data* data) {
-    //TODO
+int execute_client(int client_id, struct comm_buffers *buffers, struct main_data *data) {
+    while (true) {
+        int counter = 0;
+        // TODO
+        if (!(data->terminate)) {
+            // TODO
+            counter++;
+        }
+        else {
+            return counter;
+        }
+    }
 }
 
-void client_get_operation(struct operation* op, int client_id, struct comm_buffers* buffers, struct main_data* data) {
-    //TODO
+void client_get_operation(struct operation *op, int client_id, struct comm_buffers *buffers, struct main_data *data) {
+    if(!(data->terminate)){
+        read_main_client_buffer(buffers, client_id, data->buffers_size, op);
+    }
+    
+    return;
 }
 
-void client_process_operation(struct operation* op, int client_id, struct main_data* data, int* counter) {
-    //TODO
+void client_process_operation(struct operation *op, int client_id, struct main_data *data, int *counter) {
+    // TODO
 }
 
-void client_send_operation(struct operation* op, struct comm_buffers* buffers, struct main_data* data) {
-    //TODO
+void client_send_operation(struct operation *op, struct comm_buffers *buffers, struct main_data *data) {
+    // TODO
 }
